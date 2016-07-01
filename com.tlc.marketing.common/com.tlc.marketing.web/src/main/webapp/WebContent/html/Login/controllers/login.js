e294bdfafb97b9d100df83f255d4c7d7b9249c83
chepeiqing <chepeiqing@csii.com.cn>
2016-07-01 11:26:16 +0800

    登陆代码提交

--- com.tlc.marketing.common/com.tlc.marketing.web/src/main/webapp/WebContent/html/Login/controllers/login.js
+++ com.tlc.marketing.common/com.tlc.marketing.web/src/main/webapp/WebContent/html/Login/controllers/login.js
@@ -3,12 +3,12 @@ function LoginCtrl($rootScope,$scope,$service) {
 	$scope.doIt = function(){
 		var username = $scope.UserName;
 		if(username == undefined || username == ''){
-			new Pop("用户名错误","","请输入你的用户名");
+			new Pop("用户名错误","请输入你的用户名");
 			return;
 		}
 		var password = $scope.Password;
 		if(password == undefined || password == ''){
-			new Pop("密码错误","","请输入你的密码");
+			new Pop("密码错误","请输入你的密码");
 			return;
 		}
 		angular.element('.form').fadeOut(500);
@@ -17,9 +17,8 @@ function LoginCtrl($rootScope,$scope,$service) {
 			"UserName" : username,
 			"Password" : password
 		};
-		alert(formData);
 		$service.post2SRV("Login.do", formData,function(data) {
-			alert(data);
+			new Pop("返回数据",data. _exceptionMsg);
     	});
 	};
 }
