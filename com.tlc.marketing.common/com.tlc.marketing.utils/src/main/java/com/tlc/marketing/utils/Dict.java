73d1c4fe5db34079297e54067eb61311157b4770
chepeiqing <chepeiqing@csii.com.cn>
2016-10-08 18:01:48 +0800

    微信对接

--- com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/Dict.java
+++ com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/Dict.java
@@ -7,5 +7,12 @@ package com.tlc.marketing.utils;
  * @version 1.0 2016年7月29日 上午11:17:52 by chepeiqing (chepeiqing@icloud.com)
  */
 public class Dict {
-  public static String WXTOKEN = "wxf8b4f85f3a794e77";
+    /** 微信开发者校验token **/
+    public static String WXTOKEN = "wxf8b4f85f3a794e77";
+    /** 微信AppId号 **/
+    public static String APPID = "wxf8b4f85f3a794e77";
+    /** 微信AppSecret号 **/
+    public static String APPSECRET = "26a8614808f4ff4d6e0f22b3594c65e1";
+    /** 获取access_token填写client_credential **/
+    public static String CLIENT_CREDENTIAL = "client_credential";
 }
