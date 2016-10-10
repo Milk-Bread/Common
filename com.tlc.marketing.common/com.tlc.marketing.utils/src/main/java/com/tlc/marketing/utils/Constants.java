65180dde2136ab9cef79e4b2fd42a49d160eb6f4
chepeiqing <chepeiqing@csii.com.cn>
2016-10-10 20:29:55 +0800

    微信消息报文解析,返回报文组装

--- com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/Constants.java
+++ com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/Constants.java
@@ -4,17 +4,28 @@ package com.tlc.marketing.utils;
  * Description: 系统字典文件
  * Copyright (c) TLC.
  * All Rights Reserved.
+ *
  * @version 1.0 2016年10月8日 下午6:58:55 by chepeiqing (chepeiqing@icloud.com)
  */
 public class Constants {
-    /** 微信开发者校验token **/
+    /**
+     * 微信开发者校验token
+     **/
     public static String WXTOKEN = "wx66ba812ba25ec00a";
-    /** 微信AppId号 **/
+    /**
+     * 微信AppId号
+     **/
     public static String APPID = "wx67c2134f4935acfb";
-    /** 微信AppSecret号 **/
+    /**
+     * 微信AppSecret号
+     **/
     public static String APPSECRET = "b9d81b13ac888108be2c96723d1a9169";
-    /** 获取access_token填写client_credential **/
+    /**
+     * 获取access_token填写client_credential
+     **/
     public static String CLIENT_CREDENTIAL = "client_credential";
-    /** 二维码生成路径 **/
+    /**
+     * 二维码生成路径
+     **/
     public static String PATH_QRCODE_IMAGE = "/Users/chepeiqing/Desktop/images";
 }
--- com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/WeChat.java
+++ com.tlc.marketing.common/com.tlc.marketing.utils/src/main/java/com/tlc/marketing/utils/WeChat.java
@@ -4,16 +4,30 @@ package com.tlc.marketing.utils;
  * Description: 微信调用接口名称定义
  * Copyright (c) TLC.
  * All Rights Reserved.
+ *
  * @version 1.0 2016年10月8日 下午8:25:41 by chepeiqing (chepeiqing@icloud.com)
  */
 public class WeChat {
-    /** 获取access_token号 **/
+    /**
+     * 获取access_token号
+     **/
     public static String TOKEN = "https://api.weixin.qq.com/cgi-bin/token";
-    /** 创建二维码ticket（永久/临时） **/
+    /**
+     * 创建二维码ticket（永久/临时）
+     **/
     public static String CREAT_QRCODE_IMAGE = "https://api.weixin.qq.com/cgi-bin/qrcode/create";
-    /** 生成关注二维码 **/
+    /**
+     * 生成关注二维码
+     **/
     public static String SHOW_QRCODE = "https://mp.weixin.qq.com/cgi-bin/showqrcode";
-
+    /**
+     * 分类型获取永久素材的列表 POST
+     **/
+    public static String BATCHGET_MATERIAL = "https://api.weixin.qq.com/cgi-bin/material/batchget_material";
+    /**
+     * 获取永久素材的列表 GET
+     **/
+    public static String GET_MATERIALCOUNT = "https://api.weixin.qq.com/cgi-bin/material/get_materialcount";
 
 
 }
